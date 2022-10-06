dc30db28ae08ff50b510d866a1b26d8c3de4f42d
devarshee <ddevarshee12@gmail.com>
2022-10-06 23:41:59 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSchemeContext } from './context/context';
+import Navbar from './components/Navbar';
 
 function App() {
   const {temp} = useSchemeContext();
   return (
     <div className="App">
-      <h1 className='mx-100 w-[100%] border-2 border-solid border-black'>{temp}</h1>
+      <Navbar/>
+
       
     </div>
   );
---
+++ src/components/Navbar1.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Navbar = () => {
+  return (
+    <>
+
+    <First>
+        <InfoContainer>
+            
+        </InfoContainer>
+
+
+    </First>
+    <Second>
+
+    </Second>
+    <Third>
+
+    </Third>
+    </>
+  )
+}
+
+export default Navbar
+
+const First = styled.section`
+    
+`
+const Second = styled.section`
+    
+`
+
+const Third = styled.section`
+    
+`
+
