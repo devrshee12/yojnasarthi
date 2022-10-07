0c35e757d80445881a1c3b27dddea6b3966a45bc
devarshee <ddevarshee12@gmail.com>
2022-10-07 11:11:41 +0530

    carousel added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { useSchemeContext } from './context/context';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 
 function App() {
-  const {temp} = useSchemeContext();
+  // const {temp} = useSchemeContext();
   return (
     <div className="App">
-      <Navbar/>
-
+    {/* <> */}
+    <Navbar/>
+    <div className='sliderStyle'>
+      <Slider/>
+    </div>
+    {/* </> */}
+    temp
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import { SchemeProvider } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <SchemeProvider>
       <App />
     </SchemeProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
