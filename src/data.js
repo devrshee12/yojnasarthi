468e32ac891aed8d122610726d24b2af9445bc9e
krish <96249858+Krish020902@users.noreply.github.com>
2022-10-08 10:31:41 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data = [
+  {
+    id: 1,
+    title: "Agriculture,Rural & Environment",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FAgriculture%2CRural%20%26%20Environment.png&w=96&q=75",
+    count: 10,
+  },
+  {
+    id: 2,
+    title: "Banking,Financial Services and Insurance",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FBanking%2CFinancial%20Services%20and%20Insurance.png&w=96&q=75",
+    count: 39,
+  },
+  {
+    id: 3,
+    title: "Business & Entrepreneurship",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FBusiness%20%26%20Entrepreneurship.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 4,
+    title: "Education & Learning",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FEducation%20%26%20Learning.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 5,
+    title: "Health & Wellness",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FHealth%20%26%20Wellness.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 6,
+    title: "Housing & Shelter",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FHousing%20%26%20Shelter.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 7,
+    title: "Public Safety,Law & Justice",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FPublic%20Safety%2CLaw%20%26%20Justice.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 8,
+    title: "Science, IT & Communications",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FScience%2C%20IT%20%26%20Communications.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 9,
+    title: "Skills & Employment",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FSkills%20%26%20Employment.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 10,
+    title: "Social welfare & Empowerment",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FSocial%20welfare%20%26%20Empowerment.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 11,
+    title: "Sports & Culture",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FSports%20%26%20Culture.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 12,
+    title: "Transport & Infrastructure",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FTransport%20%26%20Infrastructure.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 13,
+    title: "Travel & Tourism",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FTravel%20%26%20Tourism.png&w=96&q=75",
+    count: 17,
+  },
+  {
+    id: 14,
+    title: "Utility & Sanitation",
+    logo:
+      "https://www.myscheme.gov.in/_next/image?url=https%3A%2F%2Fcdn.myscheme.in%2Fimages%2Fcategories%2FUtility%20%26%20Sanitation.png&w=96&q=75",
+    count: 17,
+  },
+];
+
+export default data;
